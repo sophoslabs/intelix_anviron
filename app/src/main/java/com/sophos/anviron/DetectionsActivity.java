3b8a619191148ac039f86ff0bd87fbb0e50f047b
Farhat Jahan <farhatjahan@actinium>
2019-06-26 19:10:08 +0530

    added report and detection activity pages and navigations along with required changes in main activity

--- app/src/main/java/com/sophos/anviron/DetectionsActivity.java
+++ app/src/main/java/com/sophos/anviron/DetectionsActivity.java
@@ -4,68 +4,53 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.sophos.anviron.ui.main.SectionsPagerAdapter;
 
-public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class DetectionsActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
+        setContentView(R.layout.navigation_detections);
         setNavigationViewListener();
-
     }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation, menu);
+        return true;
+    }
     private void setNavigationViewListener() {
         BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
     }
 
-    @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
         switch (item.getItemId()) {
             case R.id.navigation_scans: {
+                Intent intent = new Intent(DetectionsActivity.this, MainActivity.class);
+                startActivity(intent);
                 break;
             }
             case R.id.navigation_reports: {
-                Intent intent = new Intent(MainActivity.this, ReportsActivity.class);
+                Intent intent = new Intent(DetectionsActivity.this, ReportsActivity.class);
                 startActivity(intent);
                 break;
             }
             case R.id.navigation_detections: {
-                Intent intent = new Intent(MainActivity.this, DetectionsActivity.class);
-                startActivity(intent);
                 break;
             }
         }
         return true;
     }
-
-
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
 
 }
